620386fd6a1673577ee77f511b56161444c26411
satayyeb <salitayyeb@gmail.com>
2021-12-04 21:37:14 +0330

    ravan - color and cloud

--- source.c
+++ source.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <windows.h>
+#include <stdlib.h>
+#include <dos.h>
+#include <dir.h>
 
 //configurations
-#define DURATION 40
+#define DURATION 30
 #define DINOSAUR_X_POS 7
 #define EARTH 24
 #define LENGTH 80
@@ -28,7 +31,17 @@ clock_t last = 0;
 int status = 0; // 0= on earth / 1= jumping / 2= on air / 3= landing // 
 //it is better to have a struct for this variable :(
 
+char cloud[3][17] = {
+                    {' ',220,223,223,223,223,223,223,223,223,223,223,223,223,223,220,' '},
+                    {219,176,' ',176,' ',176,' ',176,' ',176,' ',176,' ',176,' ',176,219},
+                    {' ',223,220,220,220,220,220,220,220,220,220,220,220,220,220,223,' '},
+};
 
+char sun[6][7]={{' ','\\',' '  ,'|',' '  ,'/',' '},
+              {196,' ',220,220,220,' ',196},
+              {196,' ',219,219,219,' ',196},
+              {196,' ',223,223,223,' ',196},
+              {' ','/',' ','|',' ' , '\\',' '}};
 
 char mini_cactus[3][3] = {
     {FULL, ' ', DOWN},
@@ -51,6 +64,24 @@ char dinosaur[7][16] = {
     {' ', ' ', UP, FULL, FULL, FULL, FULL, FULL, UP, ' ', ' ', ' ', ' ', ' ', ' ', ' '},
     {' ', ' ', ' ', FULL, DOWN, ' ', FULL, DOWN, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
 };
+
+void SetColor(int ForgC)
+{
+ WORD wColor;
+
+  HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+  CONSOLE_SCREEN_BUFFER_INFO csbi;
+
+                   //We use csbi for the wAttributes word.
+ if(GetConsoleScreenBufferInfo(hStdOut, &csbi))
+ {
+             //Mask out all but the background attribute, and add in the forgournd     color
+      wColor = (csbi.wAttributes & 0xF0) + (ForgC & 0x0F);
+      SetConsoleTextAttribute(hStdOut, wColor);
+ }
+ return;
+}
+
 
 void gotoxy(int x, int y) {
     COORD coord;
@@ -114,6 +145,7 @@ void print_dinosaur() {
     }
 
     //print the dinosaur
+
     for (int i = 0; i < 7; i++) {
         for (int i = 0; i < DINOSAUR_X_POS; i++) {
             printf(" ");
@@ -135,18 +167,21 @@ int clear_cactus() {
 }
 
 int print_cactus() {
+    SetColor(2);
     for (int i = 0; i < 5; i++) {
         gotoxy(last_cactus_pos, EARTH - 5 + i);
         for (int j = 0; j < 5; j++) {
             printf("%c", cactus[i][j]);
         }
     }
+    SetColor(15);
     if ((status == 0) && (last_cactus_pos + 7) > DINOSAUR_X_POS && last_cactus_pos < DINOSAUR_X_POS + 15) {
         game_over_flag = 1;  
     }
 }
 
 void pa_zadan() {
+    SetColor(4);
     if (difftime(clock(), t2) > DURATION * 2) {
         t2 = clock();
         if (status == 0) {
@@ -159,10 +194,10 @@ void pa_zadan() {
             which_foot ^= 1;
         }
     }
+    SetColor(15);
 }
 
 int cactus_rail() {
-
     pa_zadan();
 
     if (difftime(clock(), last) > DURATION) {
@@ -196,14 +231,18 @@ int main() {
 
     //first screen
     status = 2;
+    SetColor(4);
     print_dinosaur();
+    SetColor(5);
     gotoxy(1, 4);
     printf("It worked! I have no idea why...");
     gotoxy(5, 5);
     printf("Press space to play");
     gotoxy(6, 6);
     printf("    x to exit");
+    SetColor(15);
     ch = getch();
+
     while (ch != ' ' && ch != 'x' && ch != 'X') {
         ch = getch();
     }
@@ -214,11 +253,15 @@ int main() {
 
     //init the game
     status = 0;
+    SetColor(4);
     print_dinosaur();
+    SetColor(15);
     gotoxy(0, EARTH);
+    SetColor(6);
     for (int i = 0; i < LENGTH; i++) {
         printf("%c", EARTH_MATERILAL);
     }
+    SetColor(15);
     gotoxy(3, 1);
     printf("score: ");
 
@@ -228,33 +271,102 @@ int main() {
         if (game_over_flag) {
             system("cls");
             gotoxy(5, 5);
+            SetColor(1);
             printf("U lost :(");
             gotoxy(5, 6);
             printf("Your Score: %lld", score);
             gotoxy(5, 7);
             printf("Press x to exit...");
-
+            SetColor(15);
             while (1) {
                 ch = getch();
                 if (ch == 'x' || ch == 'X')
                     return 0;
             }
         }
+        
+        //printing sun
+        
+        for (int i = 0; i < 6; i++)
+        {
+           // printf("%*s",35," ");
+            SetColor(14);
+            for (int j = 0; j < 7; j++)
+            {   
+                gotoxy(48+j,1+i);
+                printf("%c",sun[i][j]);
+            }
+            printf("\n");
+            //printf("%*.S",20," ");
+        }
+        SetColor(15);
+        //printing cloud 1&2&3
+
+        gotoxy(30,5);
+        for (int i = 0; i < 3; i++)
+        {
+           // printf("%*s",35," ");
+            SetColor(11);
+            for (int j = 0; j < 17; j++)
+            {   
+                printf("%c",cloud[i][j]);
+            }
+            printf("\n");
+            printf("%*.S",30," ");
+        }
+        SetColor(15);
+
+
+        gotoxy(60,1);
+        for (int i = 0; i < 3; i++)
+        {
+           // printf("%*s",35," ");
+            SetColor(11);
+            for (int j = 0; j < 17; j++)
+            {   
+                printf("%c",cloud[i][j]);
+            }
+            printf("\n");
+            printf("%*.S",60," ");
+        }
+        SetColor(15);
+
+
+        gotoxy(50,7);
+        for (int i = 0; i < 3; i++)
+        {
+           // printf("%*s",35," ");
+            SetColor(11);
+            for (int j = 0; j < 17; j++)
+            {   
+                printf("%c",cloud[i][j]);
+            }
+            printf("\n");
+            printf("%*.S",50," ");
+        }
+        SetColor(15);
+
 
         if (status == 1 && difftime(clock(), t) > 100) {
             t = clock();
             status = 2;
+            SetColor(4);
             print_dinosaur();
+            SetColor(15);
         }
         if (status == 2 && difftime(clock(), t) > 1100) {
             t = clock();
             status = 3;
+            SetColor(4);
             print_dinosaur();
+            SetColor(15);        
         }
         if (status == 3 && difftime(clock(), t) > 100) {
             t = clock();
             status = 0;
+            SetColor(4);
             print_dinosaur();
+            SetColor(15);
         }
 
         if (status != 0 && kbhit()) {
@@ -278,7 +390,9 @@ int main() {
         if (ch == ' ') {
             t = clock();
             status = 1;
+            SetColor(4);
             print_dinosaur();
+            SetColor(15);
         }
 
         if (ch == 'G') {
@@ -289,10 +403,11 @@ int main() {
 
         if (ch == 'H')
             score += 100;
-
         else if (ch == 'x' || ch == 'X') {
+            SetColor(14);
             system("cls");
             printf("\n\n        It worked! I have no idea why...  :P    \n\n\n\n");
+            SetColor(15);
             return (0);
         }
     }
